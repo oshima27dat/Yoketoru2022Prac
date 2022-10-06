e6196b5264c4be43a0b02598d46001fb2ff17649
A601-03 <oshima.dat22@gmail.com>
2022-10-06 15:09:33 +0900

    ゲーム開始

---
+++ Assets/Yoketoru/Scripts/ToNextScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToNextScene : MonoBehaviour
+{
+    [Tooltip("êÿÇËë÷Ç¶ÇΩÇ¢ÉVÅ[Éìñº"), SerializeField]
+    string nextScene = default;
+
+    bool sceneChanged;
+
+    public void ChangeScene()
+    {
+        if (sceneChanged) return;
+
+        sceneChanged = true;
+        TinyAudio.PlaySE(TinyAudio.SE.Start);
+        SceneManager.LoadScene(nextScene);
+    }
+}
