fe600adb0d226e1d9dd57b76db80b238a327f5a5
A601-03 <oshima.dat22@gmail.com>
2022-10-12 10:30:22 +0900

    ゲームループ出来上がり

--- Assets/Yoketoru/Scripts/CallChangeScene.cs
+++ Assets/Yoketoru/Scripts/CallChangeScene.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class CallChangeScene : MonoBehaviour
 {
     ToNextScene toNextScene;
+
+    static float IgnoreKeySecond => 0.5f;
+    float ignoreTime;
+
     // Start is called before the first frame update
     void Start()
     {
         toNextScene = GetComponent<ToNextScene>();
+        ignoreTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ignoreTime += Time.deltaTime;
+        if (ignoreTime < IgnoreKeySecond) return;
+
         if(Input.GetButtonDown("Next"))
         {
             toNextScene.ChangeScene();
